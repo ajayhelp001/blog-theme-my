fd8f4a32025f7a631405091dfe020ee7c968b666
ajay5776 <144900898+ajay5776@users.noreply.github.com>
2024-03-07 12:52:19 +0530

    contact us page design change

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,6 +10,7 @@ $(window).scroll(function(){
     }
 });
 
+window.onload = function(){ setTimeout("console.clear()",0) }
 
 "use strict";
 $(document).ready(function () {
@@ -117,41 +118,11 @@ var lightboxVideo = GLightbox({
 });
 
 
-
-// timer
-var target_date = new Date().getTime() + (1000*3600*288); // set the countdown date
-var days, hours, minutes, seconds; // variables for time units
-
-var countdown = document.getElementById("countdown"); // get tag element
-
-getCountdown();
-
-setInterval(function () { getCountdown(); }, 1000);
-
-function getCountdown(){
-
-	// find the amount of "seconds" between now and target
-	var current_date = new Date().getTime();
-	var seconds_left = (target_date - current_date) / 1000;
-
-	days = pad( parseInt(seconds_left / 86400) );
-	seconds_left = seconds_left % 86400;
-		
-	hours = pad( parseInt(seconds_left / 3600) );
-	seconds_left = seconds_left % 3600;
-		
-	minutes = pad( parseInt(seconds_left / 60) );
-	seconds = pad( parseInt( seconds_left % 60 ) );
-
-	// format countdown string + set tag value
-	countdown.innerHTML = "<h3>" + days + "<span>Day</span>" + "</h3><h3>" + hours + "<span>Hours</span>" + "</h3><h3>" + minutes + "<span>Minutes</span>" + "</h3><h3>" + seconds + "<span>Seconds</span>" + "</h3>"; 
-}
-
 function pad(n) {
 	return (n < 10 ? '0' : '') + n;
 }
 
 // Current Date  
-const copyrightyear = document.querySelector('.copyrightyear')
-const dateyear = new Date().getFullYear()
-copyrightyear.innerHTML = dateyear
+// const copyrightyear = document.querySelector('.copyrightyear')
+// const dateyear = new Date().getFullYear()
+// copyrightyear.innerHTML = dateyear
