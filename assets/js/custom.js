224f68438d35fbd194e6335640d9506d2710dce8
ajay5776 <144900898+ajay5776@users.noreply.github.com>
2024-03-07 17:41:19 +0530

    about us page design  change

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -113,6 +113,37 @@ $(document).ready(function () {
 
 });
 
+// ********* Popular Post Slider *************//
+$(".client-testimonial").owlCarousel({
+	loop: true,
+	nav: true,
+	dots: true,
+	center: true,
+	margin: 10,
+	autoplay:true,
+	lazyLoad: true,
+	autoplayTimeout:1000,
+	autoplayHoverPause:true,
+	navText: [
+		'<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" data-slot="icon" class="w-6 h-6"><path fill-rule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clip-rule="evenodd"></path></svg>',
+		'<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" data-slot="icon" class="w-6 h-6"><path fill-rule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd"></path></svg>'
+	],
+	responsive: {
+		0: {
+			items: 1
+		},
+		500: {
+			items: 1
+		},
+		992: {
+			items: 1
+		},
+		1299: {
+			items: 1
+		}
+	}
+});
+
 var lightboxVideo = GLightbox({
 	selector: '.glightbox'
 });
